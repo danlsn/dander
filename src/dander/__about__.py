136efa3403281a460a82d577cc2485140fd42265
Daniel Lawson <52593003+danlsn@users.noreply.github.com>
2024-06-12 10:25:17 +1000

    Deployed f3b91d9 with MkDocs version: 1.6.0

--- src/dander/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Daniel Lawson <daniel.lawson@rmit.edu.au>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.5"
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Daniel Lawson <daniel.lawson@rmit.edu.au>
-#
-# SPDX-License-Identifier: MIT
